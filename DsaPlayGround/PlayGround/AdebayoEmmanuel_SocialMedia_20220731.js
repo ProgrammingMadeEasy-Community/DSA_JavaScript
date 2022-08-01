5822d37ef7d8d949ad55c0b3ce2991c8d036f411
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-08-01 02:21:55 +0100

    REFACTORED solution

--- DsaPlayGround/PlayGround/AdebayoEmmanuel_SocialMedia_20220731.js
+++ DsaPlayGround/PlayGround/AdebayoEmmanuel_SocialMedia_20220731.js
@@ -5,22 +5,23 @@ GetRecipient = (input, position) => {
     for (let text of messageArray) {
         if (text.charAt(0) == '@') {
             text = text.slice(1);
-            if ((!text[text.length - 1].match(/[\w-]+/))) {
-                var txt = text.slice(0, -1);
-                userDict[key] = txt;
-            } else {
-                userDict[key] = text;
+            for (let i = 0; i < text.length; i++) {
+                if (text.charAt(i).match(/[^\w-]+/))
+                    text = text.slice(0, i);
             }
+            userDict[key] = text;
             key++;
         }
     }
     return (userDict[position] != undefined) ? userDict[position] : '""';
 }
-var message = "@User_One @UserABC! Have you seen this from @Userxyz";
+var message = "I am @Em!manuel wha@t do you want @Bis56_78!sd";
 var test = GetRecipient(message, 1);
 var test1 = GetRecipient(message, 2);
-var test2 = GetRecipient(message, 3);
-var test3 = GetRecipient(message, 4);
+message = "@Joe_Bloggs yo";
+var test2 = GetRecipient(message, 1);
+message = "Hey @Joe_Bloggs what time are we meeting @FredBloggs?";
+var test3 = GetRecipient(message, 2);
 console.log(test);
 console.log(test1);
 console.log(test2);
